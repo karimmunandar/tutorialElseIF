746f9cd1e76971c085689bd30c64415c35bb61c5
karim <karimmunandar141@gmail.com>
2019-07-22 01:04:27 +0700

    first commit

---
+++ elseif.js
@@ -0,0 +1,8 @@
+var angka = prompt('Masukan Angka:');
+if (angka % 2 == 0) {
+    alert(angka + 'adalah bilangan genap');
+} else if (angka % 2 == 1) {
+    alert(angka + 'adalah bilangan ganjil');
+} else {
+    alert('yang annda masukan Bukan angka');
+}
